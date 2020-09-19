02f3352548251cc315afd0e1d117e068423996cc
Federico Di Chena <dichena.federico@gmail.com>
2020-09-19 20:38:00 -0300

    Funcionamiento de parametrizacion de los metodos

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,8 +1,17 @@
-class Module
+
+module ContratoCondiciones
   attr_accessor :before, :after
   def before_and_after_each_method(before, after)
     @before = before
     @after = after
+  end
+
+  def pre(&pre)
+    @before = pre
+  end
+
+  def post(&post)
+    @after = post
   end
 
   def method_added(method_name)
@@ -12,30 +21,48 @@ class Module
     original_method = 'original_' + method_name.to_s
     alias_method original_method, method_name
     @methods_aliased << method_name
-    define_method method_name do
+    define_method(method_name) do
+    |*arg|
       self.class.before.call
-      send(original_method)
+      send(original_method,*arg)
       self.class.after.call
     end
   end
 
 end
 
-
-
+Module.module_eval { prepend ContratoCondiciones }
 
 class B
 
-  def saludo1
+  attr_accessor :divisor
+  #invariant { capacity >= 0 }
+  pre{divisor != 0}
+  post{divisor != 0}
+  def saludo1 nombre1, nombre2
+    puts 'chau2' + " "+  nombre1 + " " + nombre2
+  end
+
+  def saludo2
     puts 'chau2'
   end
 
-  before_and_after_each_method(proc {puts 'antes'}, proc {puts 'despues'})
+  before_and_after_each_method(proc {puts 'antes'},proc{puts 'despues'} )
 
-  def saludo3
-    puts 'chau'
+  def saludo3 nombre
+    puts 'chau' + " " + nombre
+  end
+
+  def cantidadPersonas cantidad
+    puts 4 + cantidad
   end
 end
 
-B.new.saludo1
-B.new.saludo3
+B.new.saludo1 "Carlos","Alberto"
+B.new.saludo2
+B.new.saludo3 "Carlos"
+B.new.cantidadPersonas 5
+
+
+
+
