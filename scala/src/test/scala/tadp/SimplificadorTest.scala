b574c07125979e45f05b9f2864b8621aa9318cd4
Dario <dariokozicki@gmail.com>
2020-12-05 10:25:46 -0300

    add basic simplifyers

---
+++ scala/src/test/scala/tadp/SimplificadorTest.scala
@@ -0,0 +1,49 @@
+package tadp
+
+import grupo3.ParsersTadp.{ParserException, Salida, parserEntrada}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import Simplificadores._
+import scala.util.Try
+
+class SimplificadorTest extends AnyFlatSpec with should.Matchers {
+  def testAssertVerdeYResultado[T](actualResultado: Try[Salida[T]], resultadoEsperado: Salida[T]): Unit = {
+    actualResultado.get shouldBe resultadoEsperado
+  }
+  def testAssertFallo[T](actualResultado: => Try[Salida[T]]): Unit = {
+    intercept[ParserException] { actualResultado.get }
+  }
+
+  it should "test simplificar color dentro de color" in {
+    val color = "color[255,255,255](color[255,255,255](rectangulo[10 @ 20, 30 @ 40]))"
+    val colorParsed = parserEntrada(color)
+    val colorFixed = simplificarColorAnidado(colorParsed.get._1)
+    assertResult(("color", (List(255.0,255.0,255.0), ("rectangulo", (List(List(10.0,20.0), List(30.0,40.0)))))))(colorFixed)
+  }
+
+  it should "test simplificar redundancia" in {
+    val redund = "escala[1,1](traslacion[0,0](rotacion[0](triangulo[10 @ 20, 30 @ 40, 50 @ 60])))"
+    val redundParsed = parserEntrada(redund)
+    val redundFixed = simplificar((redundParsed.get._1))
+    assertResult(("triangulo",(List(List(10.0,20.0), List(30.0,40.0), List(50.0,60.0)))))(redundFixed)
+  }
+
+  it should "test simplificar grupos" in {
+    val grupoConColor = "grupo(color[255,255,255](circulo[10 @ 20, 30]),color[255,255,255](circulo[10 @ 20, 30]),color[255,255,255](circulo[10 @ 20, 30]))"
+    val grupoParsed = parserEntrada(grupoConColor)
+    val grupoFixed = simplificar(grupoParsed.get._1)
+    assertResult(
+      ("color",
+        (List(255.0,255.0,255.0),
+          ("grupo",
+            List(
+              ("circulo",(List(10.0,20.0), 30)),
+              ("circulo",(List(10.0,20.0), 30)),
+              ("circulo",(List(10.0,20.0), 30))
+            )
+          )
+        )
+      )
+    )(grupoFixed)
+  }
+}
